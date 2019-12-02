94a42bbca78409c24d3dcb7b60e658fdf835ec6e
falms <F@L.fal.ms>
2019-12-02 21:56:46 +0900

    support MSP account

--- meraki/controllers_login/login_controller.py
+++ meraki/controllers_login/login_controller.py
@@ -3,6 +3,8 @@
 from meraki.api_helper import APIHelper
 from meraki.configuration import Configuration
 from meraki.controllers.base_controller import BaseController
+from meraki.exceptions.api_exception import APIException
+from meraki.http.auth.custom_header_auth import CustomHeaderAuth
 
 
 class LoginController(BaseController):
@@ -32,10 +34,47 @@ class LoginController(BaseController):
 
         _response = APIHelper.json_deserialize(_context.response.raw_body)
 
+        if _response.get('mode') == 'org_choose':
+            if 'eid' not in options.keys():
+                _eids = list(map(lambda org: org['eid'], _response.get('orgs')))
+                raise APIException('Please choose organization(eid) from {}.'.format(_eids), _context)
+            _response = self.org_choose(options.get('eid'))
+
+        if not _response.get('success', False):
+            raise APIException('Could not login. Current mode: {}'.format(_response.get('mode')), _context)
+
         # Set shard uri for later API calls
-        _shard_origin = 'https://n{}.meraki.com'.format(_response['orgs'][0]['shard_id'])
+        if 'eid' in options.keys():
+            _org = list(filter(lambda org: org['eid'] == options.get('eid'), _response['orgs']))[0]
+        else:
+            _org = _response['orgs'][0]
+
+        _shard_origin = 'https://n{}.meraki.com'.format(_org['shard_id'])
         Configuration.base_uri = _shard_origin + '/api/v0'
         Configuration.dash_uri = _shard_origin
+
+        return _response
+
+    def org_choose(self, eid=None):
+        _url_path = '/login/org_choose'
+        _query_builder = Configuration.dash_uri
+        _query_builder += _url_path
+        _query_url = APIHelper.clean_url(_query_builder)
+
+        _headers = {
+            'accept': 'application/json',
+            'content-type': 'application/json; charset=utf-8'
+        }
+
+        _params = {
+            'eid': eid
+        }
+
+        _request = self.http_client.post(_query_url, headers=_headers,
+                                         parameters=APIHelper.json_serialize(_params))
+        CustomHeaderAuth.apply(_request)
+        _context = self.execute_request(_request)
+        self.validate_response(_context)
 
         return APIHelper.json_deserialize(_context.response.raw_body)
 
